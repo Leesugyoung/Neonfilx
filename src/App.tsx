0b6cd11b7d17de8c963d22b686bbdf72bb502c3a
sugyoung lee <tnrud4685@naver.com>
2022-12-14 17:14:29 +0900

    neonfilx_header_setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,19 @@
-import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
-
-const Wrapper = styled(motion.div)`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-`;
-
-const Box = styled(motion.div)`
-  height: 400px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 50vw;
-  gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
-`;
-
-const Overlay = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
-  const [clickedId, setClickedId] = useState<null | string>(null);
   return (
-    <Wrapper>
-      <Grid>
-        {[1, 2, 3, 4].map(n => (
-          <Box onClick={() => setClickedId(n + "")} key={n} layoutId={n + ""} />
-        ))}
-      </Grid>
-      <AnimatePresence>
-        {/* AnimatePresence - 조건문에서 제거되는 컴포넌트를 exit에 적힌 내용으로 animate 해준다 */}
-        {clickedId ? (
-          <Overlay
-            onClick={() => setClickedId(null)}
-            initial={{ backgroundColor: "rgba(0, 0, 0, 0.0)" }}
-            animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-            exit={{ backgroundColor: "rgba(0, 0, 0, 0.0)" }}
-          >
-            <Box layoutId={clickedId} style={{ width: 400, height: 200 }} />
-          </Overlay>
-        ) : null}
-      </AnimatePresence>
-    </Wrapper>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import { createGlobalStyle } from "styled-components";
 import App from "./App";
-import { darkTheme } from "./theme";
+import { theme } from "./theme";
 
 const GlobalStyle = createGlobalStyle`
   html, body, div, span, applet, object, iframe,
@@ -62,7 +62,6 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Source Sans Pro', sans-serif;
     color: black;
     line-height: 1.2;
-    background:linear-gradient(135deg,#e09,#d0e);
   }
   a {
     text-decoration: none;
@@ -75,7 +74,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <RecoilRoot>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>

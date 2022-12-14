a6882b01d68a5eef1baa4884211ff493e6eea898
sugyoung lee <tnrud4685@naver.com>
2022-12-14 16:30:47 +0900

    framer motion practice don

--- src/App.tsx
+++ src/App.tsx
@@ -6,78 +6,59 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    // custom back > false
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 1,
-    },
-  },
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    // custom back > ture
-    opacity: 0,
-    scale: 0,
-    transition: { duration: 1 },
-  }),
-};
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-  const nextPlease = () => {
-    setBack(false);
-    setVisible(prev => (prev === 10 ? 10 : prev + 1));
-  };
-  const prevPlease = () => {
-    setBack(true);
-    setVisible(prev => (prev === 1 ? 1 : prev - 1));
-  };
+  const [clickedId, setClickedId] = useState<null | string>(null);
   return (
     <Wrapper>
-      <AnimatePresence custom={back}>
-        {/* mode="wait" > 1번 box의 exit 애니메이션이 끝난뒤에 
-        2번 box(다음 element) 가 올수있게 해준다*/}
-        <Box
-          custom={back}
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
+      <Grid>
+        {[1, 2, 3, 4].map(n => (
+          <Box onClick={() => setClickedId(n + "")} key={n} layoutId={n + ""} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {/* AnimatePresence - 조건문에서 제거되는 컴포넌트를 exit에 적힌 내용으로 animate 해준다 */}
+        {clickedId ? (
+          <Overlay
+            onClick={() => setClickedId(null)}
+            initial={{ backgroundColor: "rgba(0, 0, 0, 0.0)" }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0.0)" }}
+          >
+            <Box layoutId={clickedId} style={{ width: 400, height: 200 }} />
+          </Overlay>
+        ) : null}
       </AnimatePresence>
-      <button onClick={nextPlease}>Next</button>
-      <button onClick={prevPlease}>Prev</button>
     </Wrapper>
   );
 }

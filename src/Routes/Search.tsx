bf5663b34e9ee0a5ab1bed0f2adca97f352fad36
sugyoung lee <tnrud4685@naver.com>
2022-12-21 23:41:17 +0900

    search adding

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,4 +1,9 @@
+import { useLocation } from "react-router-dom";
+import { URLSearchParams } from "url";
+
 function Search() {
+  const location = useLocation();
+  // const keyword = new URLSearchParams(location.search).get("keyword");
   return null;
 }
 

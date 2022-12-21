6c27531f19fea4383356eab8a6730424e2c5593d
sugyoung lee <tnrud4685@naver.com>
2022-12-21 23:40:45 +0900

    search adding

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,77 +1,26 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 import { motion, useAnimation, useScroll } from "framer-motion";
-import { Link, useMatch } from "react-router-dom";
+import {
+  Link,
+  NavigateFunction,
+  useMatch,
+  useNavigate,
+} from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import * as Hed from "../styled-components/StyledHeader";
 
-const Nav = styled(motion.nav)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: fixed;
-  width: 100%;
-  top: 0;
-  font-size: 14px;
-  padding: 20px 60px;
-  color: white;
-`;
+//---------Variants
+const navVariants = {
+  top: {
+    backgroundColor: "rgba(0,0,0,0)",
+  },
+  scroll: {
+    backgroundColor: "rgba(0,0,0,1)",
+  },
+};
 
-const Column = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Logo = styled(motion.svg)`
-  margin-right: 50px;
-  width: 95px;
-  height: 25px;
-  fill: ${props => props.theme.red};
-`;
-
-const Items = styled.ul`
-  display: flex;
-  align-items: center;
-`;
-
-const Item = styled.li`
-  font-weight: 500;
-  margin-right: 20px;
-  font-size: 14;
-  color: ${props => props.theme.white.darker};
-  transition: color 0.3s ease-in-out;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  &:hover {
-    color: ${props => props.theme.white.lighter};
-  }
-`;
-
-const ItemCircle = styled(motion.span)`
-  position: absolute;
-  width: 5px;
-  height: 5px;
-  background-color: ${props => props.theme.red};
-  border-radius: 5px;
-  bottom: -10px;
-  left: 0;
-  right: 0;
-  margin: 0 auto;
-`;
-
-const Search = styled.span`
-  color: white;
-  display: flex;
-  align-items: center;
-  position: relative;
-  padding-right: 5px;
-  svg {
-    height: 25px;
-    padding-right: 10px;
-  }
-`;
-
-const logoVariants = {
+export const logoVariants = {
   normal: {
     fillOpacity: 1,
   },
@@ -84,27 +33,10 @@ const logoVariants = {
   },
 };
 
-const Input = styled(motion.input)`
-  transform-origin: right center;
-  position: absolute;
-  right: 0px;
-  padding: 5px 10px;
-  padding-left: 40px;
-  z-index: -1;
-  color: white;
-  font-size: 14px;
-  background-color: transparent;
-  border: 1px solid ${props => props.theme.white.lighter};
-`;
-
-const navVariants = {
-  top: {
-    backgroundColor: "rgba(0,0,0,0)",
-  },
-  scroll: {
-    backgroundColor: "rgba(0,0,0,1)",
-  },
-};
+//---------Interface
+interface IForm {
+  keyword: string;
+}
 
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false);
@@ -122,6 +54,12 @@ function Header() {
       }
     });
   }, [scrollY, navAnimation]);
+  const navigate: NavigateFunction = useNavigate();
+
+  const { register, handleSubmit } = useForm<IForm>();
+  const onValid = ({ keyword }: IForm) => {
+    navigate(`/search?keyword=${keyword}`);
+  };
   const toggleSearch = () => {
     if (searchOpen) {
       // 검색창이 열려있으면 닫혀지는 애니메이션
@@ -137,9 +75,9 @@ function Header() {
     setSearchOpen(prev => !prev);
   };
   return (
-    <Nav variants={navVariants} animate={navAnimation} initial={"top"}>
-      <Column>
-        <Logo
+    <Hed.Nav variants={navVariants} animate={navAnimation} initial={"top"}>
+      <Hed.Column>
+        <Hed.Logo
           variants={logoVariants}
           initial="normal"
           whileHover="active"
@@ -149,22 +87,22 @@ function Header() {
           viewBox="0 0 1024 276.742"
         >
           <motion.path d="M140.803 258.904c-15.404 2.705-31.079 3.516-47.294 5.676l-49.458-144.856v151.073c-15.404 1.621-29.457 3.783-44.051 5.945v-276.742h41.08l56.212 157.021v-157.021h43.511v258.904zm85.131-157.558c16.757 0 42.431-.811 57.835-.811v43.24c-19.189 0-41.619 0-57.835.811v64.322c25.405-1.621 50.809-3.785 76.482-4.596v41.617l-119.724 9.461v-255.39h119.724v43.241h-76.482v58.105zm237.284-58.104h-44.862v198.908c-14.594 0-29.188 0-43.239.539v-199.447h-44.862v-43.242h132.965l-.002 43.242zm70.266 55.132h59.187v43.24h-59.187v98.104h-42.433v-239.718h120.808v43.241h-78.375v55.133zm148.641 103.507c24.594.539 49.456 2.434 73.51 3.783v42.701c-38.646-2.434-77.293-4.863-116.75-5.676v-242.689h43.24v201.881zm109.994 49.457c13.783.812 28.377 1.623 42.43 3.242v-254.58h-42.43v251.338zm231.881-251.338l-54.863 131.615 54.863 145.127c-16.217-2.162-32.432-5.135-48.648-7.838l-31.078-79.994-31.617 73.51c-15.678-2.705-30.812-3.516-46.484-5.678l55.672-126.75-50.269-129.992h46.482l28.377 72.699 30.27-72.699h47.295z" />
-        </Logo>
-        <Items>
-          <Item>
+        </Hed.Logo>
+        <Hed.Items>
+          <Hed.Item>
             <Link to="/">
-              Home {homeMatch && <ItemCircle layoutId="circle" />}
+              Home {homeMatch && <Hed.ItemCircle layoutId="circle" />}
             </Link>
-          </Item>
-          <Item>
+          </Hed.Item>
+          <Hed.Item>
             <Link to="tv">
-              Series {tvMatch && <ItemCircle layoutId="circle" />}
+              Series {tvMatch && <Hed.ItemCircle layoutId="circle" />}
             </Link>
-          </Item>
-        </Items>
-      </Column>
-      <Column>
-        <Search>
+          </Hed.Item>
+        </Hed.Items>
+      </Hed.Column>
+      <Hed.Column>
+        <Hed.Search onSubmit={handleSubmit(onValid)}>
           <motion.svg
             onClick={toggleSearch}
             animate={{ x: searchOpen ? -180 : 0 }}
@@ -179,15 +117,16 @@ function Header() {
               clipRule="evenodd"
             ></path>
           </motion.svg>
-          <Input
+          <Hed.Input
+            {...register("keyword", { required: true, minLength: 2 })}
             animate={inputAnimation}
             initial={{ scaleX: 0 }}
             transition={{ type: "linear" }}
             placeholder="제목, 사람, 장르"
           />
-        </Search>
-      </Column>
-    </Nav>
+        </Hed.Search>
+      </Hed.Column>
+    </Hed.Nav>
   );
 }
 

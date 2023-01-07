07aeafae59314f00dc0c6c270eb67f213f3b9f4b
sugyoung lee <tnrud4685@naver.com>
2023-01-07 18:54:57 +0900

    Movie Slider & Detail page

--- src/Components/movies/MovieDetail.tsx
+++ src/Components/movies/MovieDetail.tsx
@@ -6,6 +6,7 @@ import * as H from "../../styled-components/StyledHome";
 import * as M from "../../styled-components/StyledModal";
 import { makeImagePath } from "../../utils/utils";
 import { getMovieCredit, getMovieDetail } from "../../apis/Movi_Ser_Api";
+import React, { useEffect } from "react";
 
 interface IDetailProps {
   category?: string;
@@ -17,7 +18,7 @@ function MovieDetail({ category, id }: IDetailProps) {
 
   // movie detail API
   const { data: detailData, isLoading: detailLoading } = useQuery<IGetDetail>(
-    ["movie", `detail`],
+    ["movie", `${category}_detail`],
     () => getMovieDetail(id)
   );
 
@@ -29,16 +30,16 @@ function MovieDetail({ category, id }: IDetailProps) {
 
   // 출연진 5명 불러오기
   const actor = creditData?.cast.slice(0, 5);
-
   // 감독 정보
   const director = creditData?.crew.find(
     people => people.known_for_department === "Directing"
   );
-
   // 영화 개봉 날짜
-  const openday_data = detailData?.release_date;
-  const sub_Openday = openday_data?.substring(0, 4);
+  const sub_Openday = detailData?.release_date.substring(0, 4);
 
+  /*  useEffect(() => {
+    console.log("렌더링 중");
+  }, []); */
   return (
     <>
       {detailLoading && creditLoading ? (
@@ -79,6 +80,7 @@ function MovieDetail({ category, id }: IDetailProps) {
             />
             <M.Poster_prevBtn onClick={() => navigate(-1)}>✕</M.Poster_prevBtn>
             <M.Poster_Title>{detailData?.title}</M.Poster_Title>
+            <M.Poster_MiniTitle>{detailData?.title}</M.Poster_MiniTitle>
             <M.Poster_infomation_top>
               <span>{sub_Openday}</span>
               {detailData?.genres.slice(0, 3).map((genre, index) => (
@@ -94,18 +96,20 @@ function MovieDetail({ category, id }: IDetailProps) {
                   : "not vote"}
               </span>
             </M.Poster_infomation_top>
-
             <M.Poster_infomation_bottom>
               <M.Poster_overview>{detailData?.overview}</M.Poster_overview>
               <M.Poster_acter_and_director>
                 <M.Poster_actor>
                   <span>Casting:</span>
-                  {actor &&
-                    actor.map(cast => <div key={cast.id}> {cast.name},</div>)}
+                  {actor?.map((cast, index) => (
+                    <div key={cast.id}>
+                      {cast.name ? cast.name : "No data"},
+                    </div>
+                  ))}
                 </M.Poster_actor>
                 <M.Poster_director>
                   <span>Director:</span>
-                  {director?.name}
+                  {director?.name ? director?.name : "No data"}
                 </M.Poster_director>
               </M.Poster_acter_and_director>
             </M.Poster_infomation_bottom>
@@ -116,4 +120,4 @@ function MovieDetail({ category, id }: IDetailProps) {
   );
 }
 
-export default MovieDetail;
+export default React.memo(MovieDetail);
--- src/Components/movies/MovieSlider.tsx
+++ src/Components/movies/MovieSlider.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as H from "../../styled-components/StyledHome";
 import { useNavigate } from "react-router-dom";
-import { AnimatePresence, Variants } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
 import { makeImagePath } from "../../utils/utils";
 import { PathMatch, useMatch } from "react-router-dom";
 import { IGetResult } from "../../apis/Movi_Ser_Api";
 import MovieDetail from "./MovieDetail";
 
 // ----------Variants----
-const RowVariants: Variants = {
+const RowVariants = {
   hidden: (isNext: boolean) => {
     return {
       x: isNext ? window.innerWidth : -window.innerWidth,
@@ -23,11 +23,12 @@ const RowVariants: Variants = {
     };
   },
 };
+
 const BoxHoverVariants = {
   initial: { scale: 1 },
   hover: {
-    scale: 1.5,
-    y: -50,
+    scale: 1.3,
+    y: -30,
     transition: {
       delay: 0.3,
       duration: 0.3,
@@ -35,17 +36,6 @@ const BoxHoverVariants = {
     },
   },
 };
-const infoVariants = {
-  hover: {
-    opacity: 1,
-    transition: {
-      delay: 0.3,
-      duration: 0.3,
-      type: "tween",
-    },
-  },
-};
-// -----------------------
 
 interface IBannerProps {
   data: IGetResult | undefined;
@@ -53,100 +43,110 @@ interface IBannerProps {
   category: string;
 }
 
-const MovieSlider: React.FC<IBannerProps> = ({ category, data, title }) => {
-  // - 슬라이더 내에 한번에 보여주고싶은 영화의 개수
-  const offset = 6;
+const MovieSlider: React.FC<IBannerProps> = React.memo(
+  ({ category, data, title }) => {
+    // - 슬라이더 내에 한번에 보여주고싶은 영화의 개수
+    const offset = 6;
 
-  // - 슬라이드 다음, 이전으로 넘기기위한 인덱스
-  const [index, setIndex] = useState(0);
+    // - 슬라이드 다음, 이전으로 넘기기위한 인덱스
+    const [index, setIndex] = useState(0);
 
-  // 슬라이드 애니메이션 방향 설정
-  const [isNext, setIsNext] = useState(true);
+    // 슬라이드 애니메이션 방향 설정
+    const [isNext, setIsNext] = useState(true);
 
-  // - leaving : 슬라이드 내에 이동중인 애니메이션이 끝났는지 확인
-  const [leaving, setLeaving] = useState(false);
-  const toggleLeaving = () => setLeaving(prev => !prev);
+    // - leaving : 슬라이드 내에 이동중인 애니메이션이 끝났는지 확인
+    const [leaving, setLeaving] = useState(false);
+    const toggleLeaving = () => setLeaving(prev => !prev);
 
-  const navigate = useNavigate();
-  const onBoxClicked = (movieId: number) => {
-    navigate(`/movies/${movieId}`);
-  };
-  const bigMovieMatch: PathMatch<string> | null = useMatch("/movies/:movieId");
+    const navigate = useNavigate();
+    const onBoxClicked = (movieId: number) => {
+      navigate(`/movies/${movieId}`);
+    };
+    const bigMovieMatch: PathMatch<string> | null =
+      useMatch("/movies/:movieId");
 
-  // - nextIndex : index state 증가 함수
-  const nextIndex = () => {
-    if (data) {
-      // 애니메이션이 아직 끝나지 않았다면
-      if (leaving) return;
-      // 총 영화 개수에서 타이틀에 걸린 영화 1개 제외한 값
-      const totalMovies = data.results.length;
-      const maxIndex = Math.floor(totalMovies / offset) - 1;
-      setIsNext(true);
-      toggleLeaving();
-      setIndex(prev => (prev >= maxIndex ? 0 : prev + 1));
-    }
-  };
+    // - nextIndex : index state 증가 함수
+    const nextIndex = () => {
+      if (data) {
+        // 애니메이션이 아직 끝나지 않았다면
+        if (leaving) {
+          return;
+        } else {
+          // 타이틀에 걸린 영화 1개 제외한 값
+          const totalMovies = data.results.length;
+          const maxIndex = Math.floor(totalMovies / offset) - 1;
 
-  // - prevIndex : index state 감소 함수
-  const prevIndex = () => {
-    if (data) {
-      if (leaving) return;
-      const totalMovies = data.results.length;
-      const maxIndex = Math.floor(totalMovies / offset) - 1;
+          toggleLeaving();
+          setIndex(prev => (prev === maxIndex ? 0 : prev + 1));
+          setIsNext(() => true);
+        }
+      }
+    };
 
-      toggleLeaving();
-      setIndex(prev => (prev === 0 ? maxIndex : prev - 1));
-      setIsNext(false);
-    }
-  };
+    // - prevIndex : index state 감소 함수
+    const prevIndex = () => {
+      if (data) {
+        if (leaving) {
+          return;
+        } else {
+          const totalMovies = data.results.length;
+          const maxIndex = Math.floor(totalMovies / offset) - 1;
 
-  //index 값에 따라 6 단위의 배열로 잘라냄
-  const resultsData = data?.results
-    .slice(1)
-    .slice(offset * index, offset * index + offset);
+          toggleLeaving();
+          setIndex(prev => (prev === 0 ? maxIndex - 1 : prev - 1));
+          setIsNext(() => false);
+        }
+      }
+    };
 
-  return (
-    <>
-      {/* -- 슬라이드 영역 -- */}
-      <H.Slider>
-        <H.Slider_Title>{title}</H.Slider_Title>
-        <AnimatePresence
-          custom={isNext}
-          initial={false}
-          onExitComplete={toggleLeaving}
-        >
-          <H.Row
-            key={index}
-            variants={RowVariants}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
+    //index 값에 따라 6 단위의 배열로 잘라냄
+    const resultsData = data?.results
+      .slice(1)
+      .slice(offset * index, offset * index + offset);
+
+    return (
+      <>
+        {/* -- 슬라이드 영역 -- */}
+        <H.Slider>
+          <H.Slider_Title>{title}</H.Slider_Title>
+          <AnimatePresence
             custom={isNext}
-            transition={{ type: "tween", duration: 1 }}
+            initial={false}
+            onExitComplete={toggleLeaving}
           >
-            {resultsData &&
-              resultsData.map((movie, index) => (
-                <H.RowBox
-                  onClick={() => onBoxClicked(movie.id)}
-                  key={index + movie.id}
-                  variants={BoxHoverVariants}
-                  initial="initial"
-                  whileHover="hover"
-                  transition={{ type: "tween" }}
-                  bgphoto={makeImagePath(
-                    movie.backdrop_path
-                      ? movie.backdrop_path
-                      : movie.poster_path
-                      ? movie.poster_path
-                      : "no image."
-                  )}
-                >
-                  <H.RowBox_Info variants={infoVariants}>
-                    <h4>{movie.title}</h4>
-                  </H.RowBox_Info>
-                </H.RowBox>
-              ))}
-          </H.Row>
+            <H.Row
+              key={category + index}
+              variants={RowVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              custom={isNext}
+              transition={{ type: "tween", duration: 1 }}
+            >
+              {resultsData &&
+                resultsData.map(movie => (
+                  <H.RowBox
+                    onClick={() => onBoxClicked(movie.id)}
+                    key={category + movie.id}
+                    variants={BoxHoverVariants}
+                    initial="initial"
+                    whileHover="hover"
+                    transition={{ type: "tween" }}
+                    bgphoto={makeImagePath(
+                      movie.backdrop_path
+                        ? movie.backdrop_path
+                        : movie.poster_path
+                        ? movie.poster_path
+                        : "no image."
+                    )}
+                  >
+                    <H.RowBox_Info variants={H.infoVariants}>
+                      <h4>{movie.title}</h4>
+                    </H.RowBox_Info>
+                  </H.RowBox>
+                ))}
+            </H.Row>
+          </AnimatePresence>
           <H.prevBtn onClick={prevIndex}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
               <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
@@ -157,17 +157,20 @@ const MovieSlider: React.FC<IBannerProps> = ({ category, data, title }) => {
               <path d="M342.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L274.7 256 105.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
             </svg>
           </H.nextBtn>
-        </AnimatePresence>
-      </H.Slider>
+        </H.Slider>
 
-      {/* -- 오버레이 영역 -- */}
-      {bigMovieMatch ? (
-        <>
-          <MovieDetail id={bigMovieMatch.params.movieId!} category={category} />
-        </>
-      ) : null}
-    </>
-  );
-};
+        {/* -- 오버레이 영역 -- */}
+        {bigMovieMatch ? (
+          <>
+            <MovieDetail
+              id={bigMovieMatch.params.movieId!}
+              category={category}
+            />
+          </>
+        ) : null}
+      </>
+    );
+  }
+);
 
 export default MovieSlider;

a2c8e4db0e687d3034c4736aa911084ece8ff42f
sugyoung lee <tnrud4685@naver.com>
2023-01-07 20:04:31 +0900

    modify url

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import TvSeries from "./Routes/Series";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Routes>
         <Route path="/" element={<Movie />}>

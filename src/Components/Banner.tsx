567a6ca10234604944fc8453659e003c53138acf
sugyoung lee <tnrud4685@naver.com>
2023-02-17 04:13:02 +0900

    Banner components

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,8 +1,7 @@
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { IGetResult } from "../apis/Movi_Ser_Api";
 import * as H from "../styled-components/StyledHome";
 import { makeImagePath } from "../utils/utils";
-import MovieSlider from "./movies/MovieSlider";
 
 interface IBannerProps {
   data: IGetResult | undefined;
